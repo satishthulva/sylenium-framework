180349ec62cb94486d767f7726a43bc03826e948
simonk <sek_99@hotmail.co.uk>
2018-07-15 19:46:22 +0100

    update local

--- gui-tests/regression_jenkins.bat
+++ gui-tests/regression_jenkins.bat
@@ -1,4 +1,4 @@
 taskkill /f /im chromedriver.exe
 taskkill /f /im geckodriver.exe
 call mvn clean
-call mvn verify -Dselenide.browser=chrome -Dthread.count=10 -Dremote=http://localhost:4444/wd/hub
+call mvn verify -Dselenide.browser=Chrome -Dthread.count=10 -Dremote=http://localhost:4444/wd/hub
