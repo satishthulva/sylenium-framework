5bb0eefd35f7568e70bb16f3223b9306b4926bd0
simonk <sek_99@hotmail.co.uk>
2018-08-20 12:16:15 +0100

    headless for travis

--- gui-tests/src/test/java/io.github.symonk/listeners/TestExecutionListener.java
+++ gui-tests/src/test/java/io.github.symonk/listeners/TestExecutionListener.java
@@ -30,7 +30,16 @@ public class TestExecutionListener implements IExecutionListener {
 
   private void configureTestRun() {
     log.info("configuring the test run!");
-    Configuration.headless = true;
+
+    if(properties.isThisRunningOnTravis()) {
+        log.info("Running on travis is enabled!, this is a travis execution");
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        final ChromeOptions travisOptions = new ChromeOptions();
+        travisOptions.addArguments("--no-sandbox");
+        travisOptions.addArguments("--disable-dev-shm-usage");
+        Configuration.browserCapabilities.merge(travisOptions);
+    }
+
     Configuration.browser = properties.selenideBrowser();
     if (properties.useSeleniumGrid()) {
       Configuration.remote = properties.seleniumGridEndpoint();
