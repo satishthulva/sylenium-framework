82db31b24f5356be6f4bc11b73eeec3483dff9ad
symonk <jackofspaces@gmail.com>
2018-10-02 08:29:04 +0100

    fixes #33

--- gui-tests/src/test/java/io.github.symonk/common/helpers/reporting/ReportHelper.java
+++ gui-tests/src/test/java/io.github.symonk/common/helpers/reporting/ReportHelper.java
@@ -4,6 +4,7 @@ import io.github.symonk.configurations.properties.FrameworkProperties;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,18 +14,15 @@ import java.nio.file.Paths;
 @Slf4j
 public class ReportHelper implements ReportInteractable {
 
-  private static final String FILE_EXISTS =
-      "environment.properties file already exists, deleting it";
-  private static final String DUPLICATE_FILE_ERROR =
-      "io error occurred when checking for duplicate files";
-  private static final String DEFAULT_TRAVIS_PATH =
-      "/home/travis/build/symonk/sylenium-framework/gui-tests/target/allure-results/environment.properties";
-  private static final String DEFAULT_LOCAL_PATH = "target\\allure-results\\environment.properties";
+  private static final String FILE_EXISTS = "environment.properties file already exists, deleting it";
+  private static final String DUPLICATE_FILE_ERROR = "io error occurred when checking for duplicate files";
+  private static final String DEFAULT_TRAVIS_PATH = File.separator + "home" + File.separator + "travis" + File.separator + "build" + File.separator + "symonk" +
+          File.separator + "sylenium-framework" + File.separator + "gui-tests" + File.separator + "target" + File.separator +
+          "allure-results" + File.separator + "environment.properties";
+  private static final String DEFAULT_LOCAL_PATH = "target" + File.separator + "allure-results" + File.separator + "environment.properties";
   private static final String PROPERTIES_HEADER = "Generated runtime properties";
-  private static final String INVALID_ARGS =
-      "provided arguments do not meet a valid test run, aborting the run";
-  private static final String IO_EXCEPTION =
-      "IO error occurred when generating or pushing the environment file";
+  private static final String INVALID_ARGS = "provided arguments do not meet a valid test run, aborting the run";
+  private static final String IO_EXCEPTION = "IO error occurred when generating or pushing the environment file";
 
   private final FrameworkProperties properties;
 
@@ -41,8 +39,7 @@ public class ReportHelper implements ReportInteractable {
   private void generateEnvironmentPropertiesFile() {
     final FileOutputStream fos;
     try {
-      final Path pathToFile =
-          Paths.get(properties.isThisRunningOnTravis() ? DEFAULT_TRAVIS_PATH : DEFAULT_LOCAL_PATH);
+      final Path pathToFile = Paths.get(properties.isThisRunningOnTravis() ? DEFAULT_TRAVIS_PATH : DEFAULT_LOCAL_PATH);
       if (!removeFileIfExists(pathToFile)) {
         Files.createDirectories(pathToFile.getParent());
       }
